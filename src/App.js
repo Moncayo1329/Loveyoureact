98411b0f31373554faeb914d1b01e79857e22677
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-04 10:10:42 +0100

    add

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" width="40px"/>
-      </header>
+        </header>
+        <h1>Fun facts about React </h1>
+        <ol>
+       <li>Was first realaed in 2013</li>
+       <li>Was originally create by Jordan Walke</li>
+       <li>Has well over 100K starts on Github</li>
+       <li>Is maintained by Facebook</li>
+       <li>Powers thousand of enterprise apps, incluiding mobile apps</li>
+        </ol>
+        <footer>
+     <small>@ 2011 Mike  development. All rights reserved</small>
+       
+        </footer>
+     
     </div>
   );
 }
